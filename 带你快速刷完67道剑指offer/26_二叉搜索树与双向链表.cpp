036ff7c3cee871c1bbefdffeea92fdf31993233d
msclam <19381181753@qq.com>
2021-10-12 00:22:28 +0800

    commit all

--- 带你快速刷完67道剑指offer/26_二叉搜索树与双向链表.cpp
+++ 带你快速刷完67道剑指offer/26_二叉搜索树与双向链表.cpp
@@ -11,28 +11,74 @@ struct TreeNode {
 	}
 };
 
+
 class Solution {
 public:
     TreeNode* Convert(TreeNode* root) {
+        // 方法三
+        if (root == nullptr) return nullptr;
+        TreeNode* head = nullptr, *pre = nullptr; 
+        stack<TreeNode*> st;
+        while (!st.empty() || root != nullptr) {
+            while (root != nullptr) {
+                st.push(root);
+                root = root->left;
+            }
+            if (!st.empty()) {
+                root = st.top();
+                st.pop();
+                if (pre != nullptr) {
+                    pre->right = root;
+                    root->left = pre;
+                } else {
+                    head = root;
+                }
+                pre = root;
+                root = root->right;
+            }
+        }
+        return head;
+    }
+    
+    TreeNode* Convert(TreeNode* root) {
         if (root == nullptr) return nullptr;
         vector<TreeNode*> res;
-        inOrder(root, res);
-        return modif(res);
-    }
-    
-    void inOrder(TreeNode* root, vector<TreeNode*> &res) {
-        if (root == nullptr) return;
-        if (root->left) inOrder(root->left, res);
-        res.push_back(root);
-        if (root->right) inOrder(root->right, res);
-    }
-    
-    TreeNode* modif(vector<TreeNode*> &res) {
+        // 方法一递归+vector
+//         inOrder(root, res);
+        // 方法二非递归+vector
+        NotinOrder(root, res);
+        
         int n = res.size();
         for (int i = 0; i < n - 1; i ++ ) {
             res[i]->right = res[i + 1];
             res[i + 1]->left = res[i];
         }
+        
         return res[0];
     }
+    
+    void inOrder(TreeNode* root, vector<TreeNode*>& res) {
+        if (root == nullptr) return ;
+        inOrder(root->left, res);
+        res.push_back(root);
+        inOrder(root->right, res);
+    }
+    
+    void NotinOrder(TreeNode* root, vector<TreeNode*>& res) {
+        if (root == nullptr) return;
+        stack<TreeNode*> st;
+        while (!st.empty() || root != nullptr) {
+            while (root != nullptr) {
+                st.push(root);
+                root = root->left;
+            }
+            if (!st.empty()) {
+                root = st.top();
+                st.pop();
+                res.push_back(root);
+                root = root->right;
+            }
+        }
+    }
 };
+    
