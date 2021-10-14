a86758e83d633af92c91b7426732ea53193a8160
msclam <19381181753@qq.com>
2021-10-14 23:57:26 +0800

    commit all

--- 带你快速刷完67道剑指offer/28_数组中出现次数超过一半的数字.cpp
+++ 带你快速刷完67道剑指offer/28_数组中出现次数超过一半的数字.cpp
@@ -9,22 +9,6 @@ public:
 //         int n = A.size();
 //         int cnt = 0;
 //         int res;
-<<<<<<< HEAD
-//         for (int i = 0; i < n; i ++ ) {
-//             if (cnt == 0) {
-//                 cnt = 1;
-//                 res = A[i];
-//             } else {
-//                 if (res == A[i]) {
-//                     cnt++;
-//                 } else {
-//                     cnt--;
-//                 }
-//             }
-//         }
-//         int tmp = count(A.begin(), A.end(), res);
-//         return tmp > A.size() / 2 ? res : 0;
-=======
 //         for (auto i : A) {
 //             if (cnt == 0) {
 //                 cnt = 1;
@@ -36,7 +20,6 @@ public:
 //         }
 //         int tmp = count(A.begin(), A.end(), res);
 //         return tmp > n / 2 ? res : 0;
->>>>>>> 1b52e793c5f93c628989990f36a3b706aa831259
         
         // 方法二
         int n = A.size();
@@ -49,7 +32,7 @@ public:
         }
         return 0;
     }
-<<<<<<< HEAD
+
 };
 
 int main() {
@@ -64,6 +47,3 @@ int main() {
     
     return 0;
 }
-=======
-};
->>>>>>> 1b52e793c5f93c628989990f36a3b706aa831259
