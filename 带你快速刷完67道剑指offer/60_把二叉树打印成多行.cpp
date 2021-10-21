64aacaabe258e97555c911e9f74130fcce86a8ca
koiman <1938181753@qq.com>
2021-10-21 20:18:19 +0800

    commit all

---
+++ 带你快速刷完67道剑指offer/60_把二叉树打印成多行.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+
+class Solution {
+public:
+        vector<vector<int>> Print(TreeNode* root) {
+            vector<vector<int>> res;
+            if (root == nullptr) return res;
+            queue<TreeNode*> q;
+            q.push(root);
+            while (!q.empty()) {
+                int n = q.size();
+                vector<int> ans;
+                for (int i = 0; i < n; i ++ ) {
+                    TreeNode* node = q.front();
+                    q.pop();
+                    ans.push_back(node->val);
+                    if (node->left) q.push(node->left);
+                    if (node->right) q.push(node->right);
+                }
+                res.push_back(ans);
+            }
+            return res;
+        }
+    
+};
