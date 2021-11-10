9963f3abcfb4ad524e3a6621192728fb199fd7b0
koiman <1938181753@qq.com>
2021-11-10 19:23:25 +0800

    commit all

---
+++ 代码随想录/2_链表/移除链表元素/203_移除链表元素.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != nullptr) {
+            if (cur->next->val == val) {
+                cur->next = cur->next->next;
+            } else {
+                cur = cur->next;
+            }
+        }
+        return dummyHead->next;
+    }
+};
---
+++ 代码随想录/2_链表/设计链表/707_设计链表.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyLinkedList {
+public:
+    struct LinkedNode {
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val):val(val), next(nullptr){}
+    };
+
+    MyLinkedList() {
+        size = 0;
+        head = new LinkedNode(0);
+    }
+    
+    int get(int index) {
+        if (index >= size || index < 0) {
+            return -1;
+        }
+        LinkedNode* cur = head->next;
+        while (index--) {
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+    
+    void addAtHead(int val) {
+        LinkedNode* node = new LinkedNode(val);
+        node->next = head->next;
+        head->next = node;
+        size++;
+    }
+    
+    void addAtTail(int val) {
+        LinkedNode* node = new LinkedNode(val);
+        LinkedNode* cur = head;
+        while (cur->next != nullptr) {
+            cur = cur->next;
+        }
+        cur->next = node;
+        size++;
+    }
+    
+    void addAtIndex(int index, int val) {
+        if (index > size || index < 0) {
+            return;
+        }
+        LinkedNode* node = new LinkedNode(val);
+        LinkedNode* cur = head;
+        while (index--) {
+            cur = cur->next;
+        }
+        node->next = cur->next;
+        cur->next = node;
+        size++;
+    }
+    
+    void deleteAtIndex(int index) {
+        if (index >= size || index < 0) {
+            return;
+        }
+        LinkedNode* cur = head;
+        while (index--) {
+            cur = cur->next;
+        }
+        cur->next = cur->next->next;
+        size--;
+    }
+private:
+    int size;
+    LinkedNode* head;
+};
