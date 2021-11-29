06aee81aed5f90a7a4eb2b85b471843638206c8b
koiman <1938181753@qq.com>
2021-11-29 10:44:00 +0800

    commit all

--- 代码随想录/10_动态规划/1_基础题目/509_斐波那契数.cpp
+++ 代码随想录/10_动态规划/1_基础题目/509_斐波那契数.cpp
@@ -15,5 +15,17 @@ public:
         }
 
         return f[n];
+
+        // if (n <= 1) return n;
+
+        // vector<int> res(2);
+        // res[0] = 0;
+        // res[1] = 1;
+        // for (int i = 2; i <= n; i ++ ) {
+        //     int sum = res[0] + res[1];
+        //     res[0] = res[1];
+        //     res[1] = sum;
+        // }
+        // return res[1];
     }
 };
